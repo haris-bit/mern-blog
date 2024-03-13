17f3311267e0c98215c4fde70df0ae9856fd554b
Haris <harisiftikhar109@gmail.com>
2024-03-14 04:15:26 +0500

    revert it back to original

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://mern-blog-backend-beta-liart.vercel.app",
+        target: "http://localhost:3000",
         secure: false,
       },
     },
