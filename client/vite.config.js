2aa8c441eccd89b6e4a75efe1774b083aa10fc22
Haris <harisiftikhar109@gmail.com>
2024-03-14 03:52:50 +0500

    backend fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://mern-blog-ebon.vercel.app",
         secure: false,
       },
     },
