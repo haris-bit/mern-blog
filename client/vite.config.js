321a5cbb7e2ae2941bf321311bc002237959540a
Haris <harisiftikhar109@gmail.com>
2024-03-15 01:12:18 +0500

    pre config installation

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://mern-blog-backend-eight.vercel.app",
+        target: "http://localhost:3000",
         secure: false,
       },
     },
--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 
 dotenv.config();
@@ -13,6 +14,9 @@ dotenv.config();
 mongoose.connect(process.env.MONGO)
 .then(() => console.log('MongoDB is Connected!'))
 .catch(err => console.log(err));
+
+
+const __dirname = path.resolve();
 
 const app = express()
 app.use(express.json());
@@ -26,6 +30,13 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
 
 
 app.get('/', (req, res) => {
