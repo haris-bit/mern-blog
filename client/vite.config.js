ad907805ca351d0c8e4224463e574005beeb0832
Haris <harisiftikhar109@gmail.com>
2024-03-14 06:27:10 +0500

    backend url config

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://mern-blog-backend-eight.vercel.app/",
+        target: "https://mern-blog-backend-eight.vercel.app",
         secure: false,
       },
     },
